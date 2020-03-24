97dea862b093ac38cbfac79ab5e74b0e32a093cc
parkseohuinim <parkseohuinim@gmail.com>
2020-03-24 16:01:22 +0900

    [#2.4 Protection with PropTypes] Study

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
+
+const foodILike = [
+  {
+    id: 1,
+    name: 'Kimchi',
+    picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRn-owKdGg249Q0W6JKMS3sj3fmA-1NAXHbmF_Mpv0936nk2CnN',
+    rating: 4.5
+  },
+  {
+    id: 2,
+    name: 'Samgyeopsal',
+    picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQjiNclNYVVilJjHmtGFoJ-AIM_-jmKxLD6XWs5DWDxaeIuBocQ',
+    rating: 4.3
+  }
+];
+
+function Food({name, picture, rating}) {
+  return <div>
+          <h2>I like {name}</h2>
+          <h4>{rating} / 5.0</h4>>
+          <img src={picture} alt={name}></img>
+        </div>;
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React2
-        </a>
-      </header>
+    <div>
+      {foodILike.map(dish => (
+        <Food 
+          key={dish.id} 
+          name={dish.name} 
+          picture={dish.picture} 
+          rating={dish.rating}>
+        </Food>
+      ))}
     </div>
   );
 }
