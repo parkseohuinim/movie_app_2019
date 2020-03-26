85f43ad70d1982b5ef9a7fa64adb1b15e36e6336
parkseohuinim <parkseohuinim@gmail.com>
2020-03-26 17:04:00 +0900

    Updates

---
+++ precache-manifest.f9af5d57aa586624ba9fddb07ef3ccd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd245a1fdec6a670bffdcf3a2943f0c0",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "fa84abb127aa37a979f2",
+    "url": "/movie_app_2019/static/css/main.798f57a5.chunk.css"
+  },
+  {
+    "revision": "30d06ccd0c335fa0bbc6",
+    "url": "/movie_app_2019/static/js/2.8281a1d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.8281a1d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa84abb127aa37a979f2",
+    "url": "/movie_app_2019/static/js/main.ef36690c.chunk.js"
+  },
+  {
+    "revision": "419d2513b13f5da474d1",
+    "url": "/movie_app_2019/static/js/runtime-main.3d564817.js"
+  }
+]);
